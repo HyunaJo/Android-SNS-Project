46355e4c8d0d7d8309982575d3fead87d72c9112
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2022-11-20 01:03:41 +0900

    게시물 작성 기능 변경

--- app/src/main/java/com/example/sns_project/AddPostFragment.kt
+++ app/src/main/java/com/example/sns_project/AddPostFragment.kt
@@ -2,37 +2,42 @@ package com.example.sns_project
 
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.sns_project.databinding.AddPostBinding
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
-import java.time.LocalDateTime
 
 class AddPostFragment: Fragment(R.layout.add_post){
     var uid = Firebase.auth.currentUser?.uid //현재 로그인한 사용자의 uid
+    var userEmail = Firebase.auth.currentUser?.email.toString()
     var uriList: ArrayList<Uri> = arrayListOf<Uri>()
     lateinit var binding:AddPostBinding
     companion object {
         //static 변수
         var boardId:Int = 1
+        var count:Int = 0
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val db = Firebase.firestore
-
-        val snsActivity = activity as SnsActivity
-        snsActivity.showUpButton()
+        val database = Firebase.database("https://sns-project-dc395-default-rtdb.asia-southeast1.firebasedatabase.app/")
 
         binding = AddPostBinding.bind(view)
+
+        val dataFormat = SimpleDateFormat("yy-MM-dd-hh:mm:ss")
 
         //갤러리에서 사진 찾기
         binding.imageSerchButton.setOnClickListener {
@@ -51,32 +56,39 @@ class AddPostFragment: Fragment(R.layout.add_post){
                 binding.imageLoadText.text = "이미지를 선택하세요."
                 return@setOnClickListener
             }
-
+            val currentTime : Long = System.currentTimeMillis() // ms로 반환
             val data = hashMapOf(
                 "uid" to uid.toString(),
+                "boardId" to boardId.toString(),
                 "post" to binding.postText.text.toString(),
-                "location" to binding.locationText.text.toString()
+                "location" to binding.locationText.text.toString(),
+                "time" to dataFormat.format(currentTime).toString()
             )
+
+
+
+            val EmailID = userEmail.split("@")
+            var boardRef = database.getReference("board")
+            //board에 새로운 게시물 추가
+            boardRef = boardRef.child(EmailID[0].toString() + "_" + boardId)
+            boardRef.setValue(data)
+
+            //user의 boardList 속성에 board 식별자 추가
+            var usersRef = database.getReference("users")
+            usersRef.child(EmailID[0]).child("boardList").child((boardId-1).toString()).setValue(EmailID[0].toString() + "_" + boardId)
 
             //이미지 Storge에 업로드
             for (i in 0 until uriList.size!!) { //선택 한 사진수만큼 반복
                 uploadImageTOFirebase(uriList[i])
+                //user의 imageList 속성에 사진 식별자 추가
+                usersRef.child(EmailID[0]).child("imageList").child((count-1).toString()).setValue(boardId.toString() + "_" + (count-1) + ".png")
             }
-
-            //게시글, 위치 Firestore에 업로드
-            db.collection("Board")
-                .add(data)
-                .addOnSuccessListener {
-                    // 성공할 경우
-                }
-                .addOnFailureListener { exception ->
-                    // 실패할 경우
-                }
 
             //게시물 식별자 증가(다음 게시물을 위해)
             boardId+=1
-        }
 
+            findNavController().navigate(R.id.action_addPostFragment_to_homeFragment)
+        }
     }
 
     //갤러리에서 고른 사진들 Uri로 변환
@@ -113,7 +125,8 @@ class AddPostFragment: Fragment(R.layout.add_post){
     fun uploadImageTOFirebase(uri: Uri?) {
         var storage: FirebaseStorage? = FirebaseStorage.getInstance()   //FirebaseStorage 인스턴스 생성
         //파일 이름 생성.
-        var fileName = uid.toString() + "_" + boardId + "_" + LocalDateTime.now() + ".png";
+//        var fileName = uid.toString() + "_" + boardId + "_" + LocalDateTime.now() + ".png";
+        var fileName = boardId.toString() + "_" + (count++) + ".png";
         //파일 업로드, 다운로드, 삭제, 메타데이터 가져오기 또는 업데이트를 하기 위해 참조를 생성.
         //참조는 클라우드 파일을 가리키는 포인터라고 할 수 있음.
         var imagesRef = storage!!.reference.child("images/").child(fileName)    //기본 참조 위치/images/${fileName}
