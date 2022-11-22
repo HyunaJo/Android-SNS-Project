1a20fdbec53e33677c17d9613af16ef5d46725b7
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2022-11-22 21:07:19 +0900

    개인 피드에 게시글 사진 출력(3x3)

--- app/src/main/java/com/example/sns_project/AddPostFragment.kt
+++ app/src/main/java/com/example/sns_project/AddPostFragment.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -26,24 +27,25 @@ class AddPostFragment: Fragment(R.layout.add_post){
     var userEmail = Firebase.auth.currentUser?.email.toString()
     var uriList: ArrayList<Uri> = arrayListOf<Uri>()
     lateinit var binding:AddPostBinding
+    var uri:Uri? = null
     companion object {
         //static 변수
         var boardId:Int = 1
         var count:Int = 0
     }
+    val dataFormat = SimpleDateFormat("yy-MM-dd-hh:mm:ss")
+    val database = Firebase.database("https://sns-project-dc395-default-rtdb.asia-southeast1.firebasedatabase.app/")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val database = Firebase.database("https://sns-project-dc395-default-rtdb.asia-southeast1.firebasedatabase.app/")
 
         binding = AddPostBinding.bind(view)
-
-        val dataFormat = SimpleDateFormat("yy-MM-dd-hh:mm:ss")
 
         //갤러리에서 사진 찾기
         binding.imageSerchButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true); //추가
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*"
+            )
             binding.imageLoadText.text = ""
             activityResult.launch(intent)
         }
@@ -52,86 +54,67 @@ class AddPostFragment: Fragment(R.layout.add_post){
         binding.postButton.setOnClickListener {
             //EditText에서 문자열 가져와 HashMap으로 생성
 
-            if(uriList.size == 0) {
+            if(uri == null) {
                 binding.imageLoadText.text = "이미지를 선택하세요."
                 return@setOnClickListener
             }
-            val currentTime : Long = System.currentTimeMillis() // ms로 반환
-            val data = hashMapOf(
-                "uid" to uid.toString(),
-                "boardId" to boardId.toString(),
-                "post" to binding.postText.text.toString(),
-                "location" to binding.locationText.text.toString(),
-                "time" to dataFormat.format(currentTime).toString()
-            )
-
-
 
             val EmailID = userEmail.split("@")
-            var boardRef = database.getReference("board")
-            //board에 새로운 게시물 추가
-            boardRef = boardRef.child(EmailID[0].toString() + "_" + boardId)
-            boardRef.setValue(data)
-
             //user의 boardList 속성에 board 식별자 추가
             var usersRef = database.getReference("users")
             usersRef.child(EmailID[0]).child("boardList").child((boardId-1).toString()).setValue(EmailID[0].toString() + "_" + boardId)
 
             //이미지 Storge에 업로드
-            for (i in 0 until uriList.size!!) { //선택 한 사진수만큼 반복
-                uploadImageTOFirebase(uriList[i])
-                //user의 imageList 속성에 사진 식별자 추가
-                usersRef.child(EmailID[0]).child("imageList").child((count-1).toString()).setValue(boardId.toString() + "_" + (count-1) + ".png")
-            }
-
-            //게시물 식별자 증가(다음 게시물을 위해)
-            boardId+=1
+            uploadImageTOFirebase(uri)
 
             findNavController().navigate(R.id.action_addPostFragment_to_homeFragment)
         }
     }
 
-    //갤러리에서 고른 사진들 Uri로 변환
-    val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == RESULT_OK && it.data != null) { //잘 가져온 경우
-            //화면에 보여주기
-            //Glide.with(this).load(uri).into(binding.findImage)
-
-            val clipData = it?.data?.clipData //Clip형태로 가져온다.
-            val clipDataSize = clipData?.itemCount
-            uriList = arrayListOf<Uri>()
-
-            if (clipData == null) { //이미지를 하나만 선택할 경우 clipData가 null이 올수 있음
-                //얻어온 이미지 uri로 바꿔서 배열에 저장
-                val selectedImageUri = it?.data?.data!!
-                uriList.add(selectedImageUri) //Uri 저장
-            }
-            else {
-                clipData.let { clipData ->
-                    for (i in 0 until clipDataSize!!) { //선택 한 사진수만큼 반복
-                        //얻어온 이미지 uri로 바꿔서 배열에 저장
-                        val selectedImageUri = clipData.getItemAt(i).uri
-                        uriList.add(selectedImageUri) //Uri 저장
-                    }
-                    binding.imageCountText.text = clipDataSize.toString() + "개 선택"
-                }
-            }
-
+    //갤러리에서 고른 사진 Uri로 변환
+    val activityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+        if(result.resultCode == RESULT_OK) {
+            uri = result.data?.data
         }
     }
 
     //Firebase Storage에 이미지를 업로드 하는 함수.
     fun uploadImageTOFirebase(uri: Uri?) {
         var storage: FirebaseStorage? = FirebaseStorage.getInstance()   //FirebaseStorage 인스턴스 생성
-        //파일 이름 생성.
-//        var fileName = uid.toString() + "_" + boardId + "_" + LocalDateTime.now() + ".png";
         var fileName = boardId.toString() + "_" + (count++) + ".png";
+        var storageRef = storage?.reference?.child("images")?.child(fileName)
         //파일 업로드, 다운로드, 삭제, 메타데이터 가져오기 또는 업데이트를 하기 위해 참조를 생성.
         //참조는 클라우드 파일을 가리키는 포인터라고 할 수 있음.
         var imagesRef = storage!!.reference.child("images/").child(fileName)    //기본 참조 위치/images/${fileName}
         //이미지 파일 업로드
         imagesRef.putFile(uri!!).addOnSuccessListener {
+            //업로드 완료된 경우 users와 board에 관련 값 저장
+            storageRef?.downloadUrl?.addOnSuccessListener { uri ->
+                val currentTime : Long = System.currentTimeMillis() // ms로 반환
+                val data = hashMapOf(
+                    "uid" to uid.toString(),
+                    "boardId" to boardId.toString(),
+                    "post" to binding.postText.text.toString(),
+                    "location" to binding.locationText.text.toString(),
+                    "time" to dataFormat.format(currentTime).toString(),
+                    "imageUrl" to uri!!.toString(),
+                )
+
+                val EmailID = userEmail.split("@")
+                //board에 새로운 게시물 식별자 추가
+                var boardRef = database.getReference("board")
+                boardRef = boardRef.child(EmailID[0].toString() + "_" + boardId)
+                boardRef.setValue(data)
+
+                //게시물 식별자 증가(다음 게시물을 위해)
+                boardId+=1
+
+                //해당 사용자의 imageList에 imageUri 추가
+                var usersRef = database.getReference("users")
+                usersRef.child(EmailID[0]).child("imageList").child((count-1).toString()).setValue(uri!!.toString())
+            }
+
         }.addOnFailureListener {
             println(it)
         }
--- app/src/main/java/com/example/sns_project/MyFeedFragment.kt
+++ app/src/main/java/com/example/sns_project/MyFeedFragment.kt
@@ -2,21 +2,34 @@ package com.example.sns_project
 
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.sns_project.databinding.MyfeedfragmentLayoutBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 
 class MyFeedFragment : Fragment(R.layout.myfeedfragment_layout) {
     lateinit var viewModel:SnsViewModel
     lateinit var binding:MyfeedfragmentLayoutBinding
+    lateinit var imageUrl:Uri
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +37,7 @@ class MyFeedFragment : Fragment(R.layout.myfeedfragment_layout) {
         savedInstanceState: Bundle?
     ): View? {
         binding = MyfeedfragmentLayoutBinding.inflate(inflater,container,false)
+        binding.recyclerview?.adapter = ImageFragmentRecyclerAdapter()
         return binding.root
     }
 
@@ -89,6 +103,57 @@ class MyFeedFragment : Fragment(R.layout.myfeedfragment_layout) {
 //            snsActivity.supportFragmentManager.beginTransaction().replace(R.id.fragment_container,FollowingListFragment()).commit()
         }
 
+    }
 
+    inner class ImageFragmentRecyclerAdapter: RecyclerView.Adapter<ImageFragmentRecyclerAdapter.ViewHolder>() {
+        val database = Firebase.database("https://sns-project-dc395-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        var imageList: ArrayList<String> = arrayListOf<String>()
+        var imageUrl: String = ""
+        init {
+            // 내 사진의 url만 찾아서 imageList에 저장
+            var userList = java.util.ArrayList<User>()
+            val myRef = database.getReference("users")
+            myRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (userSnapShot in snapshot.children) {
+                        val user = userSnapShot.getValue(User::class.java)
+                        userList.add(user!!)
+                    }
+                    var userEmail = Firebase.auth.currentUser?.email.toString()
+                    for(i in userList) {
+                        if(i.email.equals(userEmail)) {
+                            imageList = i.imageList!!
+                            imageList.reverse()
+                            System.out.println(imageList)
+                            break;
+                        }
+                    }
+                    notifyDataSetChanged()
+                }
+            })
+            System.out.println(imageList)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.myfeedimage_item, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val width = resources.displayMetrics.widthPixels / 3
+            holder.profileImage.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            Glide.with(holder.itemView.context).load(imageList[position]).into(holder.profileImage)
+            System.out.println(imageList.size)
+        }
+
+        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+            val profileImage = itemView.findViewById<ImageView>(R.id.MyFeedGridImage)
+        }
+
+        override fun getItemCount(): Int {
+            return imageList.size
+        }
     }
 }
