59b28b5b066f240841f2386f388e718568efa3ee
HyunaJo <69022662+HyunaJo@users.noreply.github.com>
2022-11-27 13:39:08 +0900

    check internet connection

--- app/src/main/java/com/example/sns_project/LoginActivity.kt
+++ app/src/main/java/com/example/sns_project/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.example.sns_project
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
 import android.util.Log
+import android.widget.Toast
 import com.example.sns_project.databinding.ActivityLoginBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -37,28 +41,51 @@ class LoginActivity : AppCompatActivity() {
             doLogin(userEmail, password)
         }
 
-        //회원가입 클릭
-        binding.signup.setOnClickListener {
-            startActivity(
-                Intent(this, CreateUserActivity::class.java) //회원가입 화면으로 이동
-            )
+        if(isOnline(applicationContext)){
+            //회원가입 클릭
+            binding.signup.setOnClickListener {
+                startActivity(
+                    Intent(this, CreateUserActivity::class.java) //회원가입 화면으로 이동
+                )
+            }
         }
+
     }
 
     //로그인 인증 메소드
     private fun doLogin(userEmail: String, password: String) {
         Firebase.auth.signInWithEmailAndPassword(userEmail, password)
             .addOnCompleteListener(this) { // it: Task<AuthResult!>
-                if (it.isSuccessful) { //로그인 성공한 경우
-                    startActivity(
-                        Intent(this, MainActivity::class.java) //메인 화면으로 이동
-                    )
-                    finish()
-                }
-                else { //로그인 실패한 경우
-                    binding.loginExceptionText.text = "아이디 또는 비밀번호를 다시 확인하세요."
-                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                if(isOnline(applicationContext)){
+                    if (it.isSuccessful) { //로그인 성공한 경우
+                        startActivity(
+                            Intent(this, MainActivity::class.java) //메인 화면으로 이동
+                        )
+                        finish()
+                    }
+                    else { //로그인 실패한 경우
+                        binding.loginExceptionText.text = "아이디 또는 비밀번호를 다시 확인하세요."
+                        Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    }
                 }
             }
+    }
+
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                return true
+            }
+        }
+        Toast.makeText(this, "인터넷 연결이 필요합니다.", Toast.LENGTH_SHORT).show()
+        return false
     }
 }
--- app/src/main/java/com/example/sns_project/MainActivity.kt
+++ app/src/main/java/com/example/sns_project/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.sns_project
 
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.sns_project.databinding.ActivityMainBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -20,23 +26,39 @@ class MainActivity : AppCompatActivity() {
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
-        System.out.println("******************* ${Firebase.auth.currentUser?.email} ***********************")
-        //로그인하지 않은 경우
-        if (Firebase.auth.currentUser == null) {
+        if(isOnline(applicationContext)){
+            //로그인하지 않은 경우
+            if (Firebase.auth.currentUser == null) {
+                startActivity(
+                    Intent(this, LoginActivity::class.java)) //로그인 화면으로 이동
+                finish()
+            }
+            else{
+                pendingIntent.send()
+            }
+        }
+        else{
             startActivity(
                 Intent(this, LoginActivity::class.java)) //로그인 화면으로 이동
             finish()
         }
-        else{
-            pendingIntent.send()
+    }
+
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                return true
+            }
         }
-
-//        //로그아웃 버튼 클릭
-//        binding.logoutButton.setOnClickListener {
-//            Firebase.auth.signOut()
-//            finish()
-//        }
-
-
+        Toast.makeText(this, "인터넷 연결이 필요합니다.", Toast.LENGTH_SHORT).show()
+        return false
     }
 }
